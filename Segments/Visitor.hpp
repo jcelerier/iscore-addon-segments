a7267dd5980c4de2fc52b2347c82b7853d9249ff
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-09-09 16:47:45 +0200

    add two examples of visitors

--- Segments/Visitor.hpp
+++ Segments/Visitor.hpp
@@ -1,120 +1,449 @@
 #pragma once
 #include <core/document/DocumentModel.hpp>
-#include <Scenario/Process/ScenarioModel.hpp>
+#include <Scenario/Document/Constraint/Rack/RackModel.hpp>
+#include <Scenario/Document/Constraint/Rack/Slot/SlotModel.hpp>
+#include <Scenario/Document/TimeNode/Trigger/TriggerModel.hpp>
 #include <Scenario/Document/ScenarioDocument/ScenarioDocumentModel.hpp>
+#include <Scenario/Document/State/ItemModel/MessageItemModel.hpp>
+#include <Scenario/Document/BaseScenario/BaseScenario.hpp>
+#include <Scenario/Process/ScenarioModel.hpp>
 #include <Loop/LoopProcessModel.hpp>
 #include <ImageProcess/ImageModel.hpp>
 #include <Audio/SoundProcess/SoundProcessModel.hpp>
 
-#include <boost/variant.hpp>
+#include <Scenario/Process/Algorithms/Accessors.hpp>
+#include <Scenario/Process/Algorithms/ContainersAccessors.hpp>
+#include <iscore/tools/SubtypeVariant.hpp>
 namespace Segments
 {
+
+/** For the sake of convenience
+ * we define here a type that allows to eaisly visit
+ * the processes that we are interested in.
+ *
+ * There can be a lot of processes since they can be
+ * implemented via plug-ins but most won't be useful
+ * for segments (midi notes, automations, etc...)
+ */
 using SegmentsProcess =
-    eggs::variant<
-        const Scenario::ProcessModel*,
-        const Loop::ProcessModel*,
-        const Image::ProcessModel*,
-        const Audio::Sound::ProcessModel*>;
+    iscore::SubtypeVariant<
+        const Process::ProcessModel,
+        const Scenario::ProcessModel,
+        const Loop::ProcessModel,
+        const Image::ProcessModel,
+        const Audio::Sound::ProcessModel>;
 
-template<typename Base, typename Variant_T>
-auto make_dynamic_variant_impl(const Base& base)
+struct DepthVisitor
 {
-    // The type does not match any
-    // in the proposed types for the variant.
-    return Variant_T{};
-}
+        QDebug stream;
+        int hierarchy_depth = 0;
 
-template<typename Base, typename Variant_T, typename Arg, typename... Args>
-auto make_dynamic_variant_impl(const Base& base)
-{
-    if(auto derived = dynamic_cast<Arg>(&base))
-    {
-        return Variant_T{derived};
-    }
+        auto& print()
+        {
+            return stream << QString(hierarchy_depth, ' ');
+        }
 
-    return make_dynamic_variant_impl<Base, Variant_T, Args...>(base);
-}
-
-template<typename Base, typename... Args>
-auto make_dynamic_variant(const Base& base)
-{
-    return make_dynamic_variant_impl<Base, eggs::variant<Args...>, Args...>(base);
-}
-
-auto make_segments_process(const Process::ProcessModel& base)
-{
-    return make_dynamic_variant<Process::ProcessModel,
-            const Scenario::ProcessModel*,
-            const Loop::ProcessModel*,
-            const Image::ProcessModel*,
-            const Audio::Sound::ProcessModel*>(base);
-}
-
-struct SegmentsVisitor
-{
         void operator()(const Scenario::ConstraintModel& constraint)
         {
+            print() << "Constraint: \n";
+            // First print the user metadata : name, label, etc.
+            (*this)(constraint.metadata());
+
+            // Then the durations :
+            print() << constraint.duration.defaultDuration() << " ";
+            print() << constraint.duration.minDuration() << " ";
+            print() << constraint.duration.maxDuration() << " \n";
+
             // A constraint has :
             // Temporal processes (ie what happens at execution).
             // For instance : automation, etc...
+            print() << "Processes: \n";
             auto& processes = constraint.processes;
-            for(auto& process : processes)
+            hierarchy_depth++;
+            for(SegmentsProcess process : processes)
             {
-                auto var = make_segments_process(process);
-                eggs::variants::apply(*this, var);
+                process.apply(*this);
+                print() << "\n";
             }
+            hierarchy_depth--;
 
             // And racks : racks are the visual organization of the processes.
             // Most of the time you only need to use the first one which is the
             // one shown by default.
+            print() << "Racks: \n";
             auto& racks = constraint.racks;
+            for(const Scenario::RackModel& rack : racks)
+            {
+                // Each rack has slots :
+                for(const Scenario::SlotModel& slot: rack.slotmodels)
+                {
+                    // Each slot has layers :
+                    for(const Process::LayerModel& layer : slot.layers)
+                    {
+                        // This allows to have multiple "views"
+                        // of the same process. For instance see
+                        // the same scenario under two aspects (one for sound and one for video).
+
+                        // But it is more for advanced i-score users :p
+                    }
+                }
+            }
+
+            // Finally, a constraint is between two states :
+            constraint.startState();
+            constraint.endState();
         }
 
         void operator()(const Scenario::EventModel& event)
         {
+            print() << "Event: \n";
+            (*this)(event.metadata());
 
+            // Events carry a condition
+            print() << "Condition:" << event.condition().toString() << "\n";
+
+            // And ids of states
+            // (they will be visited in operator()({Scenario,Loop}::ProcessModel))
+            for(auto& state_id : event.states())
+            {
+                // ...
+            }
         }
 
         void operator()(const Scenario::StateModel& state)
         {
+            print() << "State: \n";
+            (*this)(state.metadata());
 
+            // States carry messages
+            print() << "Messages: " << Process::flatten(state.messages().rootNode()) << "\n";
+
+            // And instantaneous processes.
+            // The only process currently is a javascript code one, it's not very useful for
+            // Segments I guess.
+            hierarchy_depth++;
+            for(auto& state_process : state.stateProcesses)
+            {
+                print() << "State process: " << state_process.prettyName() << "\n";
+                print() << "\n";
+            }
+            hierarchy_depth--;
         }
 
         void operator()(const Scenario::TimeNodeModel& timenode)
         {
+            print() << "TimeNode: \n";
+            (*this)(timenode.metadata());
+
+            // Time nodes are used for synchronization and also carry a condition
+            print() << "Condition:" << timenode.trigger()->expression().toString();
+
+            // And have ids of events
+            for(auto& event_id : timenode.events())
+            {
+                // ...
+            }
+        }
+
+        void operator()(const Scenario::BaseScenario& base)
+        {
+            // The top-level scenario
+            print() << "Base Scenario: \n";
+
+            hierarchy_depth++;
+            (*this)(base.startTimeNode());
+            (*this)(base.startEvent());
+            (*this)(base.startState());
+            (*this)(base.constraint());
+            (*this)(base.endState());
+            (*this)(base.endEvent());
+            (*this)(base.endTimeNode());
+            hierarchy_depth--;
+        }
+
+        // Here we list some of the processes that can be useful for Segments
+        void operator()(const Scenario::ProcessModel& scenario)
+        {
+            print() << "Scenario: \n";
+            (*this)(scenario.metadata());
+            // Scenario is the "main" process in i-score.
+            // Here we explore it "depth-first" : we check all of its elements.
+            print() << "Constraints: \n";
+            hierarchy_depth++;
+            for(auto& e : scenario.getConstraints())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+
+            print() << "TimeNodes: \n";
+            hierarchy_depth++;
+            for(auto& e : scenario.getTimeNodes())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+
+            print() << "Events: \n";
+            hierarchy_depth++;
+            for(auto& e : scenario.getEvents())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+
+            print() << "States: \n";
+            hierarchy_depth++;
+            for(auto& e : scenario.getStates())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
 
         }
 
+        void operator()(const Loop::ProcessModel& loop)
+        {
+            // Very similar to a scenario but has only one constraint
+            // and two time nodes, two events, two states
 
-        // Here we list some of the processes that can be useful for segments
-        void operator()(const Scenario::ProcessModel* scenario)
+            print() << "Loop: \n";
+            (*this)(loop.metadata());
+
+            print() << "Constraints: \n";
+            hierarchy_depth++;
+            for(auto& e : loop.getConstraints())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+
+            print() << "TimeNodes: \n";
+            hierarchy_depth++;
+            for(auto& e : loop.getTimeNodes())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+
+            print() << "Events: \n";
+            hierarchy_depth++;
+            for(auto& e : loop.getEvents())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+
+            print() << "States: \n";
+            hierarchy_depth++;
+            for(auto& e : loop.getStates())
+            {
+                (*this)(e);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+        }
+
+
+        void operator()(const iscore::ModelMetadata& metadata)
+        {
+            print() << "Metadata: "
+                    << metadata.getName() << ", "
+                    << metadata.getLabel() << ", "
+                    << metadata.getComment() << ", "
+                    << metadata.getColor().getColor() << ", "
+                    << metadata.getExtendedMetadata() << "\n";
+        }
+
+        void operator()(const Image::ProcessModel& image)
+        {
+            print() << "Image: " << image.imagePath() << "\n";
+        }
+
+        void operator()(const Audio::Sound::ProcessModel& sound)
+        {
+            print() << "Sound: " << sound.file().name() << "\n";
+        }
+
+        void operator()()
         {
 
         }
 
-        void operator()(const Loop::ProcessModel* loop)
-        {
-
-        }
-
-        void operator()(const Image::ProcessModel* image)
-        {
-
-        }
-
-        void operator()(const Audio::Sound::ProcessModel* sound)
-        {
-
-        }
 };
 
-inline void PrintScenario(const iscore::DocumentContext& doc)
+
+struct BreadthVisitor
+{
+        QDebug stream;
+        int hierarchy_depth = 0;
+
+        auto& print()
+        {
+            return stream << QString(hierarchy_depth, ' ');
+        }
+
+        void operator()(const Scenario::ConstraintModel& constraint)
+        {
+            print() << "Constraint: \n";
+            // First print the user metadata : name, label, etc.
+            (*this)(constraint.metadata());
+
+            print() << "Processes: \n";
+            auto& processes = constraint.processes;
+            hierarchy_depth++;
+            for(SegmentsProcess process : processes)
+            {
+                process.apply(*this);
+                print() << "\n";
+            }
+            hierarchy_depth--;
+
+            // Visit the next time node :
+            auto& scenar = Scenario::parentScenario(constraint);
+            (*this)(Scenario::parentTimeNode(
+                        scenar.state(constraint.endState()),
+                        scenar));
+
+            // <!> <!> <!>
+            // if two constraints end up rejoining themselves,
+            // what's after them *WILL* be visited twice.
+            // To prevent this it is necessary to add some kind of
+            // marking or list of already-visited timenodes.
+            // for instance std::unordered_set<Id<TimeNodeModel>> and check
+            // for existance before visiting.
+
+            // Since there are no cycles there cannot
+            // be infinite loops however.
+        }
+
+        void operator()(const Scenario::EventModel& event)
+        {
+            print() << "Event: \n";
+            (*this)(event.metadata());
+
+            // Visit the states
+            auto& scenar = Scenario::parentScenario(event);
+            for(auto& state_id : event.states())
+            {
+                (*this)(scenar.state(state_id));
+            }
+        }
+
+        void operator()(const Scenario::StateModel& state)
+        {
+            print() << "State: \n";
+            (*this)(state.metadata());
+
+            // Visit the next constraint
+            if(state.nextConstraint())
+            {
+                auto& scenar = Scenario::parentScenario(state);
+                (*this)(scenar.constraint(state.nextConstraint()));
+            }
+        }
+
+        void operator()(const Scenario::TimeNodeModel& timenode)
+        {
+            print() << "TimeNode: \n";
+            (*this)(timenode.metadata());
+
+            // Visit all the events
+            auto& scenar = Scenario::parentScenario(timenode);
+            for(auto& event_id : timenode.events())
+            {
+                (*this)(scenar.event(event_id));
+            }
+        }
+
+
+        void operator()(const Scenario::BaseScenario& base)
+        {
+            // The top-level scenario
+            print() << "Base Scenario: \n";
+
+            hierarchy_depth++;
+            (*this)(base.startTimeNode());
+            hierarchy_depth--;
+        }
+
+        // Processes
+        void operator()(const Scenario::ProcessModel& scenario)
+        {
+            print() << "Scenario: \n";
+            (*this)(scenario.metadata());
+
+            // Scenario is the "main" process in i-score.
+            // Here we explore it "breadth-first" : we start from
+            // the first time node and go through all the elements in the "temporal order".
+            hierarchy_depth++;
+            (*this)(scenario.startTimeNode());
+            hierarchy_depth--;
+        }
+
+        void operator()(const Loop::ProcessModel& loop)
+        {
+            print() << "Loop: \n";
+            (*this)(loop.metadata());
+
+            hierarchy_depth++;
+            (*this)(loop.startTimeNode());
+            hierarchy_depth--;
+        }
+
+        void operator()(const iscore::ModelMetadata& metadata)
+        {
+            print() << "Metadata: "
+                    << metadata.getName() << ", "
+                    << metadata.getLabel() << ", "
+                    << metadata.getComment() << ", "
+                    << metadata.getColor().getColor() << ", "
+                    << metadata.getExtendedMetadata() << "\n";
+        }
+
+        void operator()(const Image::ProcessModel& image)
+        {
+            print() << "Image: " << image.imagePath() << "\n";
+        }
+
+        void operator()(const Audio::Sound::ProcessModel& sound)
+        {
+            print() << "Sound: " << sound.file().name() << "\n";
+        }
+
+        void operator()()
+        {
+
+        }
+
+
+};
+
+inline QString PrintScenario(const iscore::DocumentContext& doc)
 {
     // First get the "root" of an i-score score.
     // A BaseScenario is the topmost hierarchy level.
-    // Scenario::BaseScenario& base = get<Scenario::ScenarioDocumentModel>(doc).baseScenario();
+    Scenario::BaseScenario& base = iscore::IDocument::get<Scenario::ScenarioDocumentModel>(doc.document).baseScenario();
 
+    QString s;
+    s.reserve(1024 * 8);
+    QDebug dbg(&s);
 
+    DepthVisitor vis{dbg.noquote().nospace()};
+    vis(base);
 
+    dbg << "\n\n\n============================\n\n\n";
+    BreadthVisitor{dbg.noquote().nospace()};
+    vis(base);
+    return s;
 }
+
+
 }
--- iscore_addon_segments.cpp
+++ iscore_addon_segments.cpp
@@ -1,15 +1,47 @@
 #include <Segments/Visitor.hpp>
+#include <iscore/menu/MenuInterface.hpp>
+#include <QInputDialog>
 #include "iscore_addon_segments.hpp"
 
-iscore_addon_segments::iscore_addon_segments() :
-    QObject {}
+namespace Segments
 {
+struct ApplicationPlugin :
+        public QObject,
+        public iscore::GUIApplicationContextPlugin
+{
+    public:
+        using iscore::GUIApplicationContextPlugin::GUIApplicationContextPlugin;
+
+        GUIElements makeGUIElements() override
+        {
+            // This will add a button in File > Export.
+            auto& m = context.menus.get().at(iscore::Menus::Export());
+            QMenu* menu = m.menu();
+
+            auto act = new QAction{QObject::tr("Segments example"), this};
+            menu->addAction(act);
+            connect(act, &QAction::triggered, this, [=] {
+                if(auto doc = this->currentDocument())
+                {
+                    QInputDialog::getMultiLineText(
+                                nullptr,
+                                "Segments",
+                                "Segments",
+                                PrintScenario(doc->context()));
+                }
+            });
+
+            return {};
+        }
+};
 }
+
+iscore_addon_segments::iscore_addon_segments() = default;
 
 iscore::GUIApplicationContextPlugin* iscore_addon_segments::make_applicationPlugin(
         const iscore::GUIApplicationContext& app)
 {
-    return nullptr;
+    return new Segments::ApplicationPlugin{app};
 }
 
 iscore::Version iscore_addon_segments::version() const
